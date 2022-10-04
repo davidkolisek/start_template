acbba605ccef71424454549124fafba2ba32f4ee
davidkolisek <kolisek.david@gmail.com>
2022-10-04 20:49:11 +0200

    template update

---
+++ js/test.js
@@ -0,0 +1,12 @@
+const func1 = () => {
+    console.log(1)
+};
+
+const func2 = () => {
+    console.log(2)
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    func1(),
+    func2()
+});
